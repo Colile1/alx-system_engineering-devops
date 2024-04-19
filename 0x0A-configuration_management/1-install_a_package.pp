1e1a5ecd93f03293fe6dd0ff10ff58886d18e4bf
Colile1 <colilesibanda@gmail.com>
2024-04-20 00:12:07 +0200

    configuration_management

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,5 @@
+# Installs a flask
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => pip3,
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# Manifest to kill process named 'killmenow'
+exec { 'pkill killmenow':
+  path => '/usr/bin:/usr/sbin:/bin',
+  onlyif => 'pgrep -f killmenow',
+  refreshonly => true,
+}
