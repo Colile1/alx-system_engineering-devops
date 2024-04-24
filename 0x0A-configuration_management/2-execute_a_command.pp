c8d03ebaa32ab6254a8b1f5520b46cb17c8d442f
Colile1 <colilesibanda@gmail.com>
2024-04-24 21:32:10 +0200

    ALX project 0x0C-web_server

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,4 @@
 # Manifest to kill process named 'killmenow'
 exec { 'pkill killmenow':
   path => '/usr/bin:/usr/sbin:/bin',
-  onlyif => 'pgrep -f killmenow',
-  refreshonly => true,
-  command  => 'pkill killmenow',
-  provider => 'shell',
 }
