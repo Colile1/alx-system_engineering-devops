554bf97396f5b729c181a47502e2e13419f6e059
Colile1 <colilesibanda@gmail.com>
2025-01-12 08:45:03 +0200

    #!/usr/bin/env bash

--- 0x0B-ssh/add_authotisation.sh
+++ 0x0B-ssh/add_authotisation.sh
@@ -3,4 +3,4 @@ mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 touch ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+cat 0x0B-ssh/3-ssh_public_key >> ~/.ssh/authorized_keys
