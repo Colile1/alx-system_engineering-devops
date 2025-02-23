e4d62e00e5a96cc33f7dad53dabae12e10aa823d
Colile1 <colilesibanda@gmail.com>
2025-02-24 01:05:12 +0200

    0

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,45 +1,24 @@
-# Puppet manifest to install and configure Nginx with a 301 redirect
+# Setting up ubuntu server with nginx using pp
 
-# Installing Nginx package
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+
 package { 'nginx':
-  ensure => installed,
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-# Creating index.html with "Hello World!" content
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-  mode    => '0644',
-  require => Package['nginx'],
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-# Configuring Nginx server block with redirect
-file { '/etc/nginx/sites-available/default':
-  content => @(END),
-    server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        root /var/www/html;
-
-        index index.html index.htm index.nginx-debian.html;
-
-        server_name _;
-
-        location / {
-            try_files \$uri \$uri/ =404;
-        }
-
-        location /redirect_me {
-            return 301  https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default;
-        }
-    }
-    | END
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => [Package['nginx'], File['/etc/nginx/sites-available/default'], File['/var/www/html/index.html']],
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
