4a80e32a87634a724ca6a57ac444b48d01b21ae5
Colile1 <colilesibanda@gmail.com>
2025-02-24 00:52:12 +0200

    puppet install

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,41 +1,45 @@
-# Install and configure Nginx web server
+# Puppet manifest to install and configure Nginx with a 301 redirect
 
-class nginx_setup {
-    package { 'nginx':
-        ensure => installed,
-    }
-
-    file { '/var/www/html/index.html':
-        content => 'Hello World!',
-    }
-
-    file { '/etc/nginx/sites-available/default':
-        content => template('nginx/default.erb'),
-        notify  => Service['nginx'],
-    }
-
-    service { 'nginx':
-        ensure    => running,
-        enable    => true,
-        subscribe => [Package['nginx'], File['/etc/nginx/sites-available/default']],
-    }
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
 }
 
-# Template: templates/nginx/default.erb
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
+# Create index.html with "Hello World!" content
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+  mode    => '0644',
+  require => Package['nginx'],
+}
 
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
+# Configure Nginx server block with redirect
+file { '/etc/nginx/sites-available/default':
+  content => @(END),
+    server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
 
-    server_name _;
+        root /var/www/html;
 
-    location / {
-        try_files \$uri \$uri/ =404;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+        location / {
+            try_files \$uri \$uri/ =404;
+        }
+
+        location /redirect_me {
+            return 301  https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default;
+        }
     }
+    | END
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
 
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => [Package['nginx'], File['/etc/nginx/sites-available/default'], File['/var/www/html/index.html']],
 }
