7c184a0f3d980522dab5a261584f0b2053681a07
colile <colilesibanda@gmail.com>
2025-04-25 08:24:56 +0200

    0x12-web stack debugging 2

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,34 @@
 # This Puppet manifest configures the SSH client to use a private key and disable password authentication.
 
+include stdlib
+
+# Remove default identity files
+file_line { 'Remove default id_rsa':
+  ensure            => absent,
+  path              => '/etc/ssh/ssh_config',
+  line              => '    IdentityFile ~/.ssh/id_rsa',
+  match             => '^    IdentityFile ~/\.ssh/id_rsa$',
+  match_for_absence => true,
+}
+
+file_line { 'Remove default id_dsa':
+  ensure            => absent,
+  path              => '/etc/ssh/ssh_config',
+  line              => '    IdentityFile ~/.ssh/id_dsa',
+  match             => '^    IdentityFile ~/\.ssh/id_dsa$',
+  match_for_absence => true,
+}
+
+# Remove any existing PasswordAuthentication
+file_line { 'Remove existing PasswordAuthentication':
+  ensure            => absent,
+  path              => '/etc/ssh/ssh_config',
+  line              => '    PasswordAuthentication',
+  match             => '^    PasswordAuthentication',
+  match_for_absence => true,
+}
+
+# Add our configuration
 file_line { 'Declare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
